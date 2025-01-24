7ed57b5f009bbebc8c8ff95127436fb8f75a0382
obogrelova <on.obogrelova@gmail.com>
2025-01-24 12:45:00 +0500

    update files

--- zerocoder/main/urls.py
+++ zerocoder/main/urls.py
@@ -1,9 +1,12 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index),
-    path('newpage2', views.newpage2),
-    path('newpage3', views.newpage3),
-    path('newpage4', views.newpage4)
-]
+    path('', views.index, name='home'),
+    path('newpage2', views.newpage2, name='page2'),
+    path('newpage3', views.newpage3, name='page3'),
+    path('newpage4', views.newpage4, name='page4')
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
